b266582d470dc30d6e59f551cdaaf0fb392a940d
Samane <s_hajizade63@yahoo.com>
2021-05-09 16:46:12 +0200

    Add multi Test for children and new movies

--- src/src/test/java/com/etraveli/RentalInfoTest.java
+++ src/src/test/java/com/etraveli/RentalInfoTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.core.Is.is;
 class RentalInfoTest {
 
     @Test
-    void showReturn() {
+    void shouldReturnMessageForRegularMovies() {
         String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
 
         String result = new RentalInfo().statement(new Customer("C. U. Stomer",
@@ -19,7 +19,28 @@ class RentalInfoTest {
                         new MovieRental("F002", 1))));
 
         assertThat(result, is(expected));
+    }
 
+    @Test
+    void shouldReturnMessageForNewMovies() {
+        String expected = "Rental Record for C. U. Stomer\n\tFast & Furious X\t3.0\n\tFast & Furious X\t12.0\nAmount owed is 15.0\nYou earned 3 frequent points\n";
+
+        String result = new RentalInfo().statement(new Customer("C. U. Stomer",
+                List.of(new MovieRental("F004", 1),
+                        new MovieRental("F004", 4))));
+
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    void shouldReturnMessageForChildrensMovies() {
+        String expected = "Rental Record for C. U. Stomer\n\tCars\t1.5\n\tCars\t3.0\nAmount owed is 4.5\nYou earned 2 frequent points\n";
+
+        String result = new RentalInfo().statement(new Customer("C. U. Stomer",
+                List.of(new MovieRental("F003", 1),
+                        new MovieRental("F003", 4))));
+
+        assertThat(result, is(expected));
     }
 
 }
